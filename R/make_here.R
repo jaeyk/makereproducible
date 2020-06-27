5d5153809a34cff478202619d35abc81191b8d9c
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2020-06-27 01:50:52 -0700

    update

--- R/make_here.R
+++ R/make_here.R
@@ -1,22 +1,22 @@
-#' make_here saves typing time to parse a relative or absolute file path into here function arguments 
+#' make_here saves typing time to parse a relative or absolute file path into here function arguments
 #'
 #' @param filepath A relative or absolute file path
-#' 
-#' @return A specified here function 
-#' @importFrom here here 
+#'
+#' @return A specified here function
+#' @importFrom here here
 #' @export
 
 make_here <- function(filepath){
-  
-  # Split the filepath 
+
+  # Split the filepath
   parsed_filepath <- strsplit(filepath, split = "[/]")
-  
-  # Extract the filename 
-  filename <- test[[1]][length(test[[1]])]
-  
-  # Extract the directory name 
-  dirname <- test[[1]][length(test[[1]]) - 1]
-  
+
+  # Extract the filename
+  filename <- parsed_filepath[[1]][length(parsed_filepath[[1]])]
+
+  # Extract the directory name
+  dirname <- parsed_filepath[[1]][length(parsed_filepath[[1]]) - 1]
+
   here(dirname, filename)
-  
+
 }
